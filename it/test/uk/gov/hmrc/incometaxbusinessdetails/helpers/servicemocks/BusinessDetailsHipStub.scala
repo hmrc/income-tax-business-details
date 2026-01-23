71cfd9e0b73bf226ffa41b697e66aa2952778458
jonathanrea <65158821+jonathanrea@users.noreply.github.com>
2026-01-23 11:07:45 +0000

    Adding IT tests

---
+++ it/test/uk/gov/hmrc/incometaxbusinessdetails/helpers/servicemocks/BusinessDetailsHipStub.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxbusinessdetails.helpers.servicemocks
+
+import uk.gov.hmrc.incometaxbusinessdetails.models.hip.incomeSourceDetails.IncomeSourceDetailsModel
+import play.api.http.Status
+import uk.gov.hmrc.incometaxbusinessdetails.constants.HipBusinessDetailsIntegrationTestConstants.{failureResponse, successResponseHip}
+import uk.gov.hmrc.incometaxbusinessdetails.constants.HipIncomeSourceIntegrationTestConstants
+import uk.gov.hmrc.incometaxbusinessdetails.helpers.WiremockHelper
+
+object BusinessDetailsHipStub {
+
+  val hipUrl: (String) => String = (mtdRef) => s"""/etmp/RESTAdapter/itsa/taxpayer/business-details?mtdReference=$mtdRef"""
+
+  def stubGetHipBusinessDetails(mtdRef: String, response: IncomeSourceDetailsModel): Unit = {
+    val hipBusinessDetailsResponse = successResponseHip(response.nino).toString
+    WiremockHelper.stubGet(hipUrl(mtdRef), Status.OK, hipBusinessDetailsResponse)
+  }
+
+  def stubGetHipBusinessDetails422NotFound(nino: String): Unit = {
+    WiremockHelper.stubGet(hipUrl(nino), Status.UNPROCESSABLE_ENTITY, HipIncomeSourceIntegrationTestConstants.errorJson422NotFoundError.toString)
+  }
+
+  def stubGetHipBusinessDetails422GenericError(nino: String): Unit = {
+    WiremockHelper.stubGet(hipUrl(nino), Status.UNPROCESSABLE_ENTITY, HipIncomeSourceIntegrationTestConstants.errorJson422GeneralError.toString)
+  }
+
+  def stubGetBusinessDetailsError(mtdRef: String): Unit = {
+    val errorResponse = failureResponse("500", "ISE")
+    WiremockHelper.stubGet(hipUrl(mtdRef), Status.INTERNAL_SERVER_ERROR, errorResponse.toString)
+  }
+
+  def verifyGetHipBusinessDetails(mtdRef: String): Unit =
+    WiremockHelper.verifyGet(hipUrl(mtdRef))
+
+}
